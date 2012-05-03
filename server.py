93a2a1e9d908d99499dc08428c4b7cd88cbb55ed
Timothy Fitz <TimothyFitz@gmail.com>
2012-05-03 17:47:17 -0400

    Add ignores for generated css/js.

--- server.py
+++ server.py
@@ -10,23 +10,16 @@ cwd = os.path.dirname(inspect.getfile(inspect.currentframe()))
 def run(command, cwd=cwd):
     return subprocess.Popen(command.split(' '), cwd=cwd)#, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-def nginx():
-    return run('python nginx.py')
-
-def solr():
-    return run('./run_solr.sh')
-
-def sass():
-    return run('sass --watch --style compressed static/scss:static/css')
-
 def main():
     run('twistd -n web --path .')
-    run('sass --watch --style compressed scss:css')
+    run('sass --watch --style compressed scss:gen_css')
     run('coffee -o gen_js/ -cw src/')
 
-    while True:
-        time.sleep(10)
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        print "Shutting down"
 
-    
 if __name__ == '__main__':
     main()
